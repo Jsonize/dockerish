238e94fc569dc35921d8fe2f9e7da4cd55d186af
Oliver Friedmann <oliver.friedmann@gmail.com>
2018-05-27 09:55:42 -0400

    Allow combination of stop build and run

--- dockerish.js
+++ dockerish.js
@@ -97,98 +97,130 @@ var target = Yaml.safeLoad(compiledTarget);
 if (parsedArgs.options.debug)
     console.log(target);
 
-var dockerArgs = null;
+var tasks = [];
 
-var tempFiles = [];
 
 if (parsedArgs.options.stop) {
-    dockerArgs = target.run.restart ? ["rm", "-f"] : ["stop"];
-    dockerArgs.push(target.container.image);
-} else if (parsedArgs.options.run || parsedArgs.options.runc) {
-    dockerArgs = ["run"];
-    var targetrun = parsedArgs.options.runc ? target[parsedArgs.options.runc] : target.run;
-    targetrun = targetrun || {};
-    if (targetrun.daemon)
-        dockerArgs.push("-d");
-    if (targetrun.restart)
-        dockerArgs.push("--restart=" + targetrun.restart);
-    else
-        dockerArgs.push("--rm");
-    if (targetrun.portmaps) {
-        targetrun.portmaps.forEach(function (portmap) {
-            dockerArgs.push("-p");
-            dockerArgs.push(portmap.host + ":" + portmap.container);
-        });
-    }
-    if (targetrun.mounts) {
-        targetrun.mounts.forEach(function (mount) {
-            dockerArgs.push("-v");
-            var mountHost = Path.resolve(mount.placeholder ? mountPlaceholders[mount.placeholder] : mount.host);
-            dockerArgs.push(mountHost + ":" + mount.container + ":" + mount.permission);
-            if (mount.permission.indexOf("w") >= 0 && !FS.existsSync(mountHost))
-                FS.writeFileSync(mountHost, "");
-        });
-    }
-    if (targetrun.privileged)
-        dockerArgs.push("--privileged");
-    if (target.container.image) {
-        dockerArgs.push("--name");
+    tasks.push(function (next) {
+        var dockerArgs = target.run.restart ? ["rm", "-f"] : ["stop"];
         dockerArgs.push(target.container.image);
-    }
-    if (targetrun.interactive)
-        dockerArgs.push("-i");
-    else
-        dockerArgs.push("-t");
-    dockerArgs.push(target.container.name);
-    if (targetrun.command)
-        dockerArgs = dockerArgs.concat(targetrun.command.split(" "));
-} else if (parsedArgs.options.build) {
-    var targetDir = Path.dirname(targetFile) + (target.container.basedir ? "/" + target.container.basedir : "");
-    if (target.dockerfile.symlinks) {
-        target.dockerfile.symlinks.forEach(function (symlink) {
-            var tempFile = Tmp.tmpNameSync({
-                template: targetDir + "/symlink-tmp-XXXXXX"
-            }) + ".tar";
-            tempFiles.push(tempFile);
-            ChildProcess.execSync("tar -C " + symlink + " -cvf " + tempFile + " . 2> /dev/null");
-            target.dockerfile.commands = target.dockerfile.commands.replace(symlink, tempFile);
-        });
-    }
-    var dockerfileLines = [
-        "FROM " + target.dockerfile.from,
-        "MAINTAINER " + target.dockerfile.maintainer
-    ].concat(target.dockerfile.commands.split("\n"));
-    var tempDockerfile = Tmp.tmpNameSync({
-        template: targetDir + "/Dockerfile-tmp-XXXXXX"
+        dockerArgs = dockerArgs.concat(parsedArgs.argv);
+        if (parsedArgs.options.debug)
+            console.log("docker", dockerArgs.join(" "));
+        var docker = ChildProcess.spawn("docker", dockerArgs);
+        docker.on("close", next);
+        docker.stderr.pipe(process.stderr);
+        docker.stdout.pipe(process.stdout);
     });
-    tempFiles.push(tempDockerfile);
-    FS.writeFileSync(tempDockerfile, dockerfileLines.join("\n"));
-    dockerArgs = [
-        "build",
-        "-f",
-        tempDockerfile,
-        "-t",
-        target.container.name,
-        targetDir
-    ];
 }
 
-if (dockerArgs) {
-    dockerArgs = dockerArgs.concat(parsedArgs.argv);
-    if (parsedArgs.options.debug)
-        console.log("docker", dockerArgs.join(" "));
-    var docker = ChildProcess.spawn("docker", dockerArgs);
-    docker.on("close", function (status) {
-        tempFiles.forEach(function (f) {
-            FS.unlinkSync(f);
+
+if (parsedArgs.options.build) {
+    tasks.push(function (next) {
+        var tempFiles = [];
+        var targetDir = Path.dirname(targetFile) + (target.container.basedir ? "/" + target.container.basedir : "");
+        if (target.dockerfile.symlinks) {
+            target.dockerfile.symlinks.forEach(function (symlink) {
+                var tempFile = Tmp.tmpNameSync({
+                    template: targetDir + "/symlink-tmp-XXXXXX"
+                }) + ".tar";
+                tempFiles.push(tempFile);
+                ChildProcess.execSync("tar -C " + symlink + " -cvf " + tempFile + " . 2> /dev/null");
+                target.dockerfile.commands = target.dockerfile.commands.replace(symlink, tempFile);
+            });
+        }
+        var dockerfileLines = [
+            "FROM " + target.dockerfile.from,
+            "MAINTAINER " + target.dockerfile.maintainer
+        ].concat(target.dockerfile.commands.split("\n"));
+        var tempDockerfile = Tmp.tmpNameSync({
+            template: targetDir + "/Dockerfile-tmp-XXXXXX"
         });
-        process.exit(status);
-    });
-    docker.stderr.pipe(process.stderr);
-    docker.stdout.pipe(process.stdout);
-    process.stdin.pipe(docker.stdin);
-} else {
-    tempFiles.forEach(function (f) {
-        FS.unlinkSync(f);
+        tempFiles.push(tempDockerfile);
+        FS.writeFileSync(tempDockerfile, dockerfileLines.join("\n"));
+        var dockerArgs = [
+            "build",
+            "-f",
+            tempDockerfile,
+            "-t",
+            target.container.name,
+            targetDir
+        ];
+        dockerArgs = dockerArgs.concat(parsedArgs.argv);
+        if (parsedArgs.options.debug)
+            console.log("docker", dockerArgs.join(" "));
+        var docker = ChildProcess.spawn("docker", dockerArgs);
+        docker.on("close", function (status) {
+            tempFiles.forEach(function (f) {
+                FS.unlinkSync(f);
+            });
+            next(status);
+        });
+        docker.stderr.pipe(process.stderr);
+        docker.stdout.pipe(process.stdout);
     });
 }
+
+
+if (parsedArgs.options.run || parsedArgs.options.runc) {
+    tasks.push(function (next) {
+        var dockerArgs = ["run"];
+        var targetrun = parsedArgs.options.runc ? target[parsedArgs.options.runc] : target.run;
+        targetrun = targetrun || {};
+        if (targetrun.daemon)
+            dockerArgs.push("-d");
+        if (targetrun.restart)
+            dockerArgs.push("--restart=" + targetrun.restart);
+        else
+            dockerArgs.push("--rm");
+        if (targetrun.portmaps) {
+            targetrun.portmaps.forEach(function (portmap) {
+                dockerArgs.push("-p");
+                dockerArgs.push(portmap.host + ":" + portmap.container);
+            });
+        }
+        if (targetrun.mounts) {
+            targetrun.mounts.forEach(function (mount) {
+                dockerArgs.push("-v");
+                var mountHost = Path.resolve(mount.placeholder ? mountPlaceholders[mount.placeholder] : mount.host);
+                dockerArgs.push(mountHost + ":" + mount.container + ":" + mount.permission);
+                if (mount.permission.indexOf("w") >= 0 && !FS.existsSync(mountHost))
+                    FS.writeFileSync(mountHost, "");
+            });
+        }
+        if (targetrun.privileged)
+            dockerArgs.push("--privileged");
+        if (target.container.image) {
+            dockerArgs.push("--name");
+            dockerArgs.push(target.container.image);
+        }
+        if (targetrun.interactive)
+            dockerArgs.push("-i");
+        else
+            dockerArgs.push("-t");
+        dockerArgs.push(target.container.name);
+        if (targetrun.command)
+            dockerArgs = dockerArgs.concat(targetrun.command.split(" "));
+        dockerArgs = dockerArgs.concat(parsedArgs.argv);
+        if (parsedArgs.options.debug)
+            console.log("docker", dockerArgs.join(" "));
+        var docker = ChildProcess.spawn("docker", dockerArgs);
+        docker.on("close", next);
+        docker.stderr.pipe(process.stderr);
+        docker.stdout.pipe(process.stdout);
+        process.stdin.pipe(docker.stdin);
+    });
+}
+
+
+var taskExecute = function (status) {
+    if (tasks.length > 0) {
+        var task = tasks.shift();
+        task(taskExecute);
+    } else {
+        process.exit(status);
+    }
+};
+
+
+taskExecute(0);
